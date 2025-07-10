096bb939cfce43500d54dd0567924923b1934adf
will chen <willchen0011@gmail.com>
2025-07-10 11:29:20 +0800

    tiny fix

--- web/src/components/Sidebar/ItemMenu.vue
+++ web/src/components/Sidebar/ItemMenu.vue
@@ -157,11 +157,6 @@ const props = defineProps({
 
 defineEmits(['action', 'close'])
 
-// Icon definitions for menu items
-const iconPaths = {
-  cluster: 'M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z'
-}
-
 // Computed properties for menu visibility
 const showEdit = computed(() => {
   return !(props.type === 'plugins' && props.item.type === 'local')
