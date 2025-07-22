97dd7f4b395dc42808e6bf3794dd33e96234fb7e
will chen <willchen0011@gmail.com>
2025-07-22 15:03:05 +0800

    update demo project and config for check suspicious ips

--- config/plugin/checkSuspiciousIPs.go
+++ config/plugin/checkSuspiciousIPs.go
@@ -10,16 +10,8 @@ var lock = &sync.RWMutex{}
 var suspiciousIpsList = make(map[string]bool)
 
 var defaultSuspiciousIpsList = map[string]bool{
-	"204.79.46.27":    true,
-	"29.37.14.55":     true,
-	"130.198.28.202":  true,
-	"151.119.173.220": true,
-	"28.105.225.46":   true,
-	"254.72.24.166":   true,
-	"63.215.82.210":   true,
-	"222.165.122.9":   true,
-	"99.94.123.210":   true,
-	"13.33.10.105":    true,
+	"1.2.3.4": true,
+	"1.2.3.5": true,
 }
 
 func getSuspiciousIps() {
